f818724581baa932434bcf4760dc2eb591054dd3
Stoyan Shopov <stoyan.shopov@gmail.com>
2020-12-21 14:50:03 +0200

    Support targed voltage measurement, and srst signal driving

--- src/platforms/stlinkv3/platform.c
+++ src/platforms/stlinkv3/platform.c
@@ -18,7 +18,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/* This file implements the platform specific functions for the ST-Link
+/* This file provides the platform specific functions for the ST-Link V3
  * implementation.
  */
 
@@ -35,10 +35,11 @@
 #include <libopencm3/usb/usbd.h>
 #include <libopencm3/stm32/adc.h>
 #include <libopencm3/stm32/spi.h>
+#include <libopencm3/stm32/adc.h>
 
 uint16_t led_idle_run;
 uint16_t srst_pin;
-static uint32_t rev;
+static uint32_t hw_version;
 
 #define SCB_CCR_IC_Pos                      17U                                           /*!< SCB CCR: Instruction cache enable bit Position */
 #define SCB_CCR_IC_Msk                     (1UL << SCB_CCR_IC_Pos)                        /*!< SCB CCR: Instruction cache enable bit Mask */
@@ -63,25 +64,25 @@ static uint32_t rev;
 
 static void __DSB(void)
 {
-  asm volatile ("dsb 0xF":::"memory");
+	asm volatile ("dsb 0xF":::"memory");
 }
 
 static void __ISB(void)
 {
-  asm volatile ("isb 0xF":::"memory");
+	asm volatile ("isb 0xF":::"memory");
 }
 
 static void SCB_EnableICache (void)
 {
 	volatile uint32_t *SCB_ICIALLU =  (volatile uint32_t *)(SCB_BASE + 0x250);
-    __DSB();
-    __ISB();
-    *SCB_ICIALLU = 0UL;                     /* invalidate I-Cache */
-    __DSB();
-    __ISB();
-    SCB_CCR |=  (uint32_t)SCB_CCR_IC_Msk;  /* enable I-Cache */
-    __DSB();
-    __ISB();
+	__DSB();
+	__ISB();
+	*SCB_ICIALLU = 0UL;                     /* invalidate I-Cache */
+	__DSB();
+	__ISB();
+	SCB_CCR |=  (uint32_t)SCB_CCR_IC_Msk;  /* enable I-Cache */
+	__DSB();
+	__ISB();
 }
 
 static void SCB_EnableDCache (void)
@@ -90,55 +91,72 @@ static void SCB_EnableDCache (void)
 	volatile uint32_t *SCB_CSSELR = (volatile uint32_t *)(SCB_BASE +  0x84);
 	volatile uint32_t *SCB_DCISW  =  (volatile uint32_t *)(SCB_BASE + 0x260);
 
-    uint32_t ccsidr;
-    uint32_t sets;
-    uint32_t ways;
+	uint32_t ccsidr;
+	uint32_t sets;
+	uint32_t ways;
 
-    *SCB_CSSELR = 0U; /*(0U << 1U) | 0U;*/  /* Level 1 data cache */
-    __DSB();
+	*SCB_CSSELR = 0U; /*(0U << 1U) | 0U;*/  /* Level 1 data cache */
+	__DSB();
 
-    ccsidr = *SCB_CCSIDR;
+	ccsidr = *SCB_CCSIDR;
 
-    sets = (uint32_t)(CCSIDR_SETS(ccsidr));
-    do {
-      ways = (uint32_t)(CCSIDR_WAYS(ccsidr));
-      do {
-        *SCB_DCISW = (((sets << SCB_DCISW_SET_Pos) & SCB_DCISW_SET_Msk) |
-                      ((ways << SCB_DCISW_WAY_Pos) & SCB_DCISW_WAY_Msk)  );
-        #if defined ( __CC_ARM )
-          __schedule_barrier();
-        #endif
-      } while (ways-- != 0U);
-    } while(sets-- != 0U);
-    __DSB();
+	sets = (uint32_t)(CCSIDR_SETS(ccsidr));
+	do {
+		ways = (uint32_t)(CCSIDR_WAYS(ccsidr));
+		do {
+			*SCB_DCISW = (((sets << SCB_DCISW_SET_Pos) & SCB_DCISW_SET_Msk) |
+					((ways << SCB_DCISW_WAY_Pos) & SCB_DCISW_WAY_Msk)  );
+#if defined ( __CC_ARM )
+			__schedule_barrier();
+#endif
+		} while (ways-- != 0U);
+	} while(sets-- != 0U);
+	__DSB();
 
-    SCB_CCR |=  (uint32_t)SCB_CCR_DC_Msk;  /* enable D-Cache */
+	SCB_CCR |=  (uint32_t)SCB_CCR_DC_Msk;  /* enable D-Cache */
 
-    __DSB();
-    __ISB();
+	__DSB();
+	__ISB();
 }
 
 int platform_hwversion(void)
 {
-	return rev;
+	return hw_version;
 }
 
 void platform_srst_set_val(bool assert)
 {
-	// TODO: fill this in
-	(void)assert;
+	gpio_set_val(SRST_PORT, SRST_PIN, !assert);
+	if (assert)
+		for(int i = 0; i < 10000; i++)
+			asm("nop");
 }
 
 bool platform_srst_get_val()
 {
-	// TODO: fill this in
-	return 0;
+	return gpio_get(SRST_PORT, SRST_PIN) == 0;
 }
 
 const char *platform_target_voltage(void)
 {
-	// TODO: fill this in
-	static char ret[] = "3.23V";
+	/* On the stlinkv3, the target input voltage is divided by two.
+	 * The ADC is sampling at 12 bit resolution. 
+	 * Vref+ input is assumed to be 3.3 volts. */
+	static char ret[] = "0.0V";
+	uint8_t channels[] = { ADC_CHANNEL0, };
+	unsigned value;
+
+	adc_set_regular_sequence(ADC1, 1, channels);
+	adc_start_conversion_regular(ADC1);
+	while (!adc_eoc(ADC1));
+	value = adc_read_regular(ADC1);
+
+	value *= 3379; /* 3.3 * 1024 == 3379.2 */
+	value += 104858; /* round, 0.05V * 2 ^ 21 == 104857.6 */
+	ret[0] = (value >> 21) + '0';
+	value &= (1 << 21) - 1;
+	value *= 10;
+	ret[2] = (value >> 21) + '0';
 
 	return ret;
 }
@@ -154,7 +172,6 @@ void platform_request_boot(void)
 
 void platform_init(void)
 {
-
 	rcc_periph_clock_enable(RCC_APB2ENR_SYSCFGEN);
 	rcc_clock_setup_hse(rcc_3v3 + RCC_CLOCK_3V3_216MHZ, 25);
 	SCB_EnableICache();
@@ -164,6 +181,18 @@ void platform_init(void)
 	rcc_periph_clock_enable(RCC_GPIOD);
 	rcc_periph_clock_enable(RCC_GPIOH);
 	rcc_periph_clock_enable(RCC_GPIOF);
+
+	/* Initialize ADC. */
+	gpio_mode_setup(GPIOA, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO0);
+	rcc_periph_clock_enable(RCC_ADC1);
+	adc_power_off(ADC1);
+	adc_disable_scan_mode(ADC1);
+	adc_set_sample_time(ADC1, ADC_CHANNEL0, ADC_SMPR_SMP_3CYC);
+	adc_power_on(ADC1);
+
+	/* Configure srst pin. */
+	gpio_set_output_options(SRST_PORT, GPIO_OTYPE_OD, GPIO_OSPEED_2MHZ, SRST_PIN);
+	gpio_mode_setup(SRST_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, SRST_PIN);
   
 	TMS_SET_MODE();
 	/* Configure TDI pin. */
@@ -180,5 +209,5 @@ void platform_init(void)
 	cdcacm_init();
 	usbuart_init();
 	/* By default, do not drive the swd bus too fast. */
-	platform_max_frequency_set(2000000);
+	platform_max_frequency_set(6000000);
 }
